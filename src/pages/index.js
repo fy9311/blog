cff396f5a2dba7bbfc9d3e06ed96c708367606cb
fy <455245435@qq.com>
2023-02-18 18:03:46 +0800

    feat: 首页修改

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
 			description="Description will go into a meta tag in <head />"
 		>
 			<HomepageHeader />
-			<main>
+			<main style={{ margin: "auto" }}>
 				{/* <HomepageFeatures /> */}
 				<img src={logo} style={{ margin: "0 auto", display: "block" }} />
 			</main>
